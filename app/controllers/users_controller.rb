e3cab96d48d041b882451f5c8ae4cc80ebd600c5
Jacob Atzen <jacob@incremental.dk>
2011-02-15 23:27:17 +0100

    Allow UsersController to respond to json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
+  respond_to :html, :xml, :json
+
   # GET /users
   # GET /users.xml
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
+    respond_with(@users)
   end
 
   # GET /users/1
