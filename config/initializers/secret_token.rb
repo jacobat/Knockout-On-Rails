4ab036f81afe184970c6daa50fae75818cad51cd
Jacob Atzen <jacob@incremental.dk>
2011-02-15 16:40:12 +0100

    Knockout On Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knockoutonrails::Application.config.secret_token = '12a62b189a0e306b3b483c7130200481544befbc2cf43e7514706e65d47c09b4f27b1de1a854330fdb4da207ae7efb476e4dbc56d7bed7d2e8768e3a71b49a59'
