d00c94c3a287e7e30eeb723a3f178e4cf7fbe470
Jacob Atzen <jacob@incremental.dk>
2011-02-15 18:16:18 +0100

    Implement basic user listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def user_path_js(user_id)
+    user_path('$USERID').gsub('$USERID', user_id)
+  end
+  def edit_user_path_js(user_id)
+    user_path('$USERID').gsub('$USERID', user_id)
+  end
 end
