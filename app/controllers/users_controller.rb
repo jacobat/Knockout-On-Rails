dfe282c6ca6446d8f85874e826d27a7c85e456bb
Jacob Atzen <jacob@incremental.dk>
2011-02-18 17:00:31 +0100

    Add etag support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,10 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
-    respond_with(@users)
+    if(stale?(:etag => User.etag))
+      @users = User.all
+      respond_with(@users)
+    end
   end
 
   # GET /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  class << self
+    def etag
+      [
+        User.order("updated_at DESC").first,
+        User.order("updated_at DESC").count
+      ]
+    end
+  end
 end
