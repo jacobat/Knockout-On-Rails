66967b88839baf353cfdcc82a13034ae979015fa
Jacob Atzen <jacob@incremental.dk>
2011-02-18 15:55:13 +0100

    Add a little seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create(:name => "Bill Gates", :email => "bill@microsoft.com")
+User.create(:name => "Steve Jobs", :email => "steve@apple.com")
+User.create(:name => "Larry Wall", :email => "wall@perl.org")
+User.create(:name => "Douglas Crockford", :email => "crockford@javascript.com")
+User.create(:name => "Yuki", :email => "yuki@example.com")
+User.create(:name => "Ona", :email => "ona@example.com")
